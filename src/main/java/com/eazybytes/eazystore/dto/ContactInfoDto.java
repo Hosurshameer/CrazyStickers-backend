e3449dd2c6fe059f2ac5fba62d737fbc68656727
Hosurshameer <shameerhosur29@gmail.com>
2026-03-25 08:36:48 +0530

    ...

--- src/main/java/com/eazybytes/eazystore/dto/ContactInfoDto.java
+++ src/main/java/com/eazybytes/eazystore/dto/ContactInfoDto.java
@@ -4,4 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "contact")
 public record ContactInfoDto(String phone,String email,String address) {
+
 }
